c41ac48abbf03d901fe40ecca74728eb5be55989
Alvin Mei <alvinkmei@gmail.com>
2023-06-20 20:05:44 -0400

    Polishing registration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import CustomLoginView, RegistrationView
+from .views import RegistrationView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("login", CustomLoginView.as_view(), name="login"),
+    path("login", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path(
+        "logout", LogoutView.as_view(template_name="users/logout.html"), name="logout"
+    ),
     path("register", RegistrationView.as_view(), name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 from django.views import View
 
 
 # Create your views here.
-class CustomLoginView(LoginView):
-    template_name = "users/login.html"
-    redirect_authenticated_user = True
-
-
 class RegistrationView(View):
     form_class = UserCreationForm
     template_name = "users/register.html"
