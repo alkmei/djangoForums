e62ebee10e95a9338d44ae1bc89c09b656ccd6a2
Alvin Mei <alvinkmei@gmail.com>
2023-06-28 17:39:07 -0400

    post reply form

--- forums/views.py
+++ forums/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Forum, Thread
+from .forms import PostForm
 
 
 def forum_index(request):
@@ -12,11 +13,23 @@ def forum_detail(request, forum_slug):
     forum = get_object_or_404(Forum, slug=forum_slug)
     threads = forum.thread_set.all()
     context = {"threads": threads, "forum": forum}
-    return render(request, "forum.html", context)
+    return render(request, "forums/forum.html", context)
 
 
 def thread_detail(request, forum_slug, thread_slug):
     thread = get_object_or_404(Thread, slug=thread_slug, forums__slug=forum_slug)
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.thread = thread
+            post.creator = request.user
+            post.save()
+            return redirect("thread_detail", thread_id=thread.id)
+    else:
+        form = PostForm()
+
     posts = thread.post_set.all()
-    context = {"thread": thread, "posts": posts}
-    return render(request, "thread.html", context)
+    context = {"thread": thread, "posts": posts, "form": form}
+    return render(request, "forums/thread.html", context)
